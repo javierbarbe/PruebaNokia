bc1e850e225deaa0b7d09a498db43016d6ecd7a4
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2021-05-07 10:46:38 +0200

    comentarios sobre almacenController.js

--- src/controllers/almacenController.js
+++ src/controllers/almacenController.js
@@ -1,6 +1,6 @@
 const modelos = require("../models");
 
-
+// funcon para recuperar todos los almacenes a una variable
 const getAlmacenes = 
     async (req, res) => {
         try {
@@ -24,6 +24,8 @@ const deleteAlmacen =async (req, res) => {
     const cantidad = await modelos.hubs.destroy({
       where: { id: idAlmacen },
     });
+        // al ser un id unico... no tiene mucho sentido esta comprobación... si acaso hacer un find, comprobar que 
+        // devuelve solo uno y ya despues realizar el destroy...
     console.log(cantidad, "debe seri igual a 1");
     if (cantidad == 1) {
       res.status(200).redirect("/listaAlmacenes");
@@ -44,6 +46,7 @@ const deleteAlmacen =async (req, res) => {
       });
       var alm = JSON.parse(JSON.stringify(almacen));
       console.log(alm, "el almacen cuya ide es" + idAlmacen);
+        // renderizacion de la vista pasando el almacen como parámetro
       res.status(200).render("almacenes/Almacen", {
         almacen: alm,
       });
@@ -67,6 +70,7 @@ const deleteAlmacen =async (req, res) => {
         }
       );
       console.log(modificado, "el modificado");
+        // redireccion a peticion GET ya carga todo ahi
       res.status(200).redirect("/listaAlmacenes");
     } catch (error) {
      console.log("ha habido un error", error);
@@ -83,14 +87,18 @@ const deleteAlmacen =async (req, res) => {
       const { nombre, localidad } = req.body;
       const almacen = await modelos.hubs.create({ nombre, localidad });
       console.log(almacen);
+        // redireccion a peticion GET ya carga todo ahi
       return res.redirect("/listaAlmacenes");
     } catch (error) {
       console.log("ha habido un error", error);
       return res.status(500).json(error);
     }
   }
-module.exports={getAlmacenes,deleteAlmacen,getEditAlmacen,postEditAlmacen,
-    getCrearAlmacen,postCrearAlmacen,
-
-
+module.exports={
+    getAlmacenes,
+    deleteAlmacen,
+    getEditAlmacen,
+    postEditAlmacen,
+    getCrearAlmacen,
+    postCrearAlmacen
 }
